668055b148b7381e9e57ea0a2cdea7826b75fdca
Hakim, Mohammad Imaduddin <mohammad.hakim@stud.th-luebeck.de>
2019-11-16 15:06:10 +0100

    implemented evaluator

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,54 +1,5 @@
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
-import java.util.List;
-
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        String csvFile = "src/main/resources/labels0.csv";
-        BufferedReader br = null;
-        String line;
-        String cvsSplitBy = ",";
 
-        List<Point> labels = new ArrayList<>();
-
-        try {
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
-                int[] coordinate = Arrays.stream(line.split(cvsSplitBy)).mapToInt(Integer::parseInt).toArray();
-                labels.add(new Point(coordinate[0], coordinate[1]));
-            }
-            System.out.println(labels);
-            int closestDistance = 999;
-            for(int i = 0; i < labels.size(); i++) {
-                for(int i2 = i+1; i2 < labels.size(); i2++) {
-                    int distance = (int) calculateDistance(labels.get(i), labels.get(i2));
-                    if (distance < closestDistance && distance > 0) {
-                        closestDistance = distance;
-                    }
-                }
-            }
-            System.out.println("closest distance: " + closestDistance);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private static double calculateDistance(Point a, Point b) {
-        double ac = Math.abs(a.y - b.y);
-        double cb = Math.abs(a.x - b.x);
-
-        return Math.hypot(ac, cb);
     }
 }
--- src/main/java/model/Evaluator.java
+++ src/main/java/model/Evaluator.java
@@ -1,4 +1,53 @@
 package model;
 
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Evaluator {
+    private double recall;
+    private double precision;
+    private double fScore;
+
+    public void calculateRecall(Set<Point> labels, Set<Point> found) {
+        Set<Point> set = intersection(labels, found);
+        recall = (double) set.size()/labels.size();
+    }
+
+    public void calculatePrecision(Set<Point> labels, Set<Point> found) {
+        Set<Point> set = intersection(labels, found);
+        precision = (double) set.size()/found.size();
+    }
+
+    public void calculateFScore(Set<Point> labels, Set<Point> found) {
+        fScore = (2*precision*recall)/(precision+recall);
+    }
+
+    private static Set<Point> intersection(Set<Point> a, Set<Point> b) {
+        if (a.size() > b.size()) {
+            return intersection(b, a);
+        }
+
+        Set<Point> results = new HashSet<>();
+
+        for (Point element : a) {
+            if (b.contains(element)) {
+                results.add(element);
+            }
+        }
+
+        return results;
+    }
+
+    public double getRecall() {
+        return recall;
+    }
+
+    public double getPrecision() {
+        return precision;
+    }
+
+    public double getfScore() {
+        return fScore;
+    }
 }
